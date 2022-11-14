184ca2e81daecd7c1f57e55d021823a74c1c0941
javier.quifer@gmail.com <javier.quifer@gmail.com>
2022-11-14 17:20:25 -0400

    suma elementos matriz

---
+++ suma_elementos_matriz.py
@@ -0,0 +1,12 @@
+mat = [[3, 8],
+       [6, 5],
+       [1, 7]]
+
+# Suma de los elemento de una matriz
+suma = 0
+for fila in mat:
+    for dato in fila:
+        suma = suma + dato
+
+print(suma)
+
